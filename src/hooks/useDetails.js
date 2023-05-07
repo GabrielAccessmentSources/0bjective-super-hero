7b24764f95affe4f1ed7d381ba2014e37ec96415
gabriel savian <gabrielsvnzrd@gmail.com>
2023-05-07 11:49:05 -0300

    feat: secure custom hook

---
+++ src/hooks/useDetails.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+// import marvel from "marvel";
+import { ts, pubKey, hash } from "../helpers/secrets";
+
+export default () => {
+    // const [details, setDetails] = useState([]);
+    // const [errorMessage, setErrorMessage] = useState("");
+    //
+    // const searchDetails = async({heroId}) => {
+    //     try {
+    //         // const response = await marvel.get(`characters?ts=${ts}&apikey=${pubKey}&hash=${hash}`)
+    //
+    //     } catch (error) {
+    //         setErrorMessage("Something went wrong")
+    //     }
+    // };
+
+    // return [searchDetails, details, errorMessage];
+};
--- src/screens/HeroDetailScreen.js
+++ src/screens/HeroDetailScreen.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet, FlatList, Image} from "react-native";
 
+import useDetails from "../hooks/useDetails";
+
 export const HeroDetailScreen = ({ navigation }) => {
-    const [result, setResult] = useState(null);
+    // const [searchDetails, details, errorMessage] = useDetails();
 
-    const id = navigation.getParam('id');
-
+    // const id = navigation.getParam('id');
+    //
+    // console.log(id)
     return (
         <View>
 
